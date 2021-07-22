e6c9c255dc6488d590a7f157fedcfe159edcf400
Vishnu Kumar <vishnu.hestabit@gmail.com>
2021-07-22 20:10:33 +0530

    fles

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+const del = require('del');
+
+gulp.task('styles', () => {
+    return gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('clean', () => {
+    return del([
+        'css/main.css',
+    ]);
+});
+
+gulp.task('default', gulp.series(['clean', 'styles']));
